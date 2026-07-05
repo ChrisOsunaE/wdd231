b27e9582a9762032611b724f43470074351a1cc9
Chris Osuna <obedosunae@gmail.com>
2026-07-04 19:01:31 -0700

    add current year and last modified date to footer

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,12 @@
 const navButton = document.querySelector('#nav-button');
 const navLinks = document.querySelector('#nav-bar');
+const currentYear = new Date().getFullYear();
+
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navLinks.classList.toggle('show');
 });
+
+document.getElementById('currentYear').textContent = currentYear;
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
